1a81ca36861199ff275faf3ee02294cc9e1338a4
Maximus Chen <Mr.MaximusChen@gmail.com>
2023-04-10 17:31:06 -0700

    W3D1. End of day with D.Han.

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,13 @@
-var VideoList = () => (
+import VideoListEntry from '/compiled/src/components/VideoListEntry.js';
+
+var VideoList = (props) => (
+
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.videos.map((video, index) => {
+      return (<div><VideoListEntry key={index} video={video}/></div>) })
+    }
   </div>
-);
+)
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
